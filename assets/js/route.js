7803e6afb8ee4a6aeecda2b6a35c13f40ae56f05
Roman Lazarev <lazarevrd59@mail.ru>
2024-12-06 19:32:45 +0300

    changed route.js (added search, filter with server)

--- assets/js/route.js
+++ assets/js/route.js
@@ -6,16 +6,12 @@ const wrapper = document.getElementById('wrapper');
 const header = document.getElementById('header');
 const footer = document.getElementById('footer');
 const paginationContainer = document.getElementById('pagination');
-
-loader.id = 'loader';
-loader.innerHTML = "<img src='./img/25.gif' alt='loader'></img>";
-dataContainer.appendChild(loader);
-
-searchInput.style.display = 'none';
-dropDown.style.display = 'none';
-header.style.display = 'none';
-footer.style.display = 'none';
-document.getElementById('checkbox').style.display = 'none';
+const filterByDistrictSelect = document.getElementById('filterByDistrict');
+const filterByTypeSelect = document.getElementById('filterByType');
+const filterBlock = document.getElementById('filters');
+const detailsContainer = document.getElementById('details');
+const gallery = document.getElementById('gallery');
+const galleryImage = document.getElementById('galleryImage');
 
 let allData = [];
 let currentPage = 1;
@@ -24,211 +20,241 @@ let sortBy = '';
 let order = '';
 let filterByDistrict = '';
 let filterByType = '';
+let currentImages = [];
+let currentImageIndex = 0;
 
-function fetchData(page, sortBy = '', order = '') {
-    dataContainer.appendChild(loader);
-    searchInput.style.display = 'none';
-    dropDown.style.display = 'none';
-    header.style.display = 'none';
-    footer.style.display = 'none';
-    document.getElementById('checkbox').style.display = 'none';
+loader.id = 'loader';
+filterBlock.className = 'route__filter-block';
+loader.innerHTML = "<img src='./img/25.gif' alt='loader'></img>";
+dataContainer.appendChild(loader);
 
-    const urlParams = new URLSearchParams();
-    urlParams.append('page', page);
-    urlParams.append('limit', itemsPerPage);
-    if (sortBy && order) {
-        urlParams.append('sortBy', sortBy);
-        urlParams.append('order', order);
-    }
+function fetchData(page, sortBy = '', order = '', filterByDistrict = '', filterByType = '', searchTerm = '') {
+  dataContainer.appendChild(loader);
+  searchInput.style.display = 'none';
+  dropDown.style.display = 'none';
+  header.style.display = 'none';
+  footer.style.display = 'none';
+  filterBlock.style.display = 'none';
+  document.getElementById('checkbox').style.display = 'none';
 
-    const url = `https://672b170d976a834dd0258e17.mockapi.io/api/v1/tourism?${urlParams.toString()}`;
+  const urlParams = new URLSearchParams();
+  urlParams.append('page', page);
+  urlParams.append('limit', itemsPerPage);
+  if (sortBy && order) {
+    urlParams.append('sortBy', sortBy);
+    urlParams.append('order', order);
+  }
+  if (filterByDistrict) {
+    urlParams.append('district', filterByDistrict);
+  }
+  if (filterByType) {
+    urlParams.append('type', filterByType);
+  }
+  if (searchTerm) {
+    urlParams.append('search', searchTerm);
+  }
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.length > 0) {
-                allData = data;
-                displayData(allData);
-                dataContainer.removeChild(loader);
-                dataContainer.style.display = 'block';
-                searchInput.style.display = 'block';
-                dropDown.style.display = 'block';
-                header.style.display = 'block';
-                footer.style.display = 'block';
-                document.getElementById('checkbox').style.display = 'block';
-                setTimeout(() => {
-                    dropDown.classList.add('show');
-                }, 10);
-            } else {
-                currentPage = currentPage - 1;
-                fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
-            }
-        })
-        .catch(error => {
-            dataContainer.removeChild(loader);
-            console.error('Ошибка загрузки данных', error);
-        });
+  const url = `https://672b170d976a834dd0258e17.mockapi.io/api/v1/tourism?${urlParams.toString()}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      dataContainer.style.display = 'block';
+      searchInput.style.display = 'block';
+      dropDown.style.display = 'block';
+      header.style.display = 'block';
+      footer.style.display = 'block';
+      filterBlock.style.display = 'block';
+      document.getElementById('checkbox').style.display = 'block';
+      if (data.length > 0) {
+        allData = data;
+        displayData(allData);
+        dataContainer.removeChild(loader);
+        setTimeout(() => {
+          dropDown.classList.add('show');
+        }, 10);
+      } else {
+        fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
+        currentPage = currentPage - 1;
+      }
+    })
+    .catch(error => {
+      dataContainer.removeChild(loader);
+      console.error('Ошибка загрузки данных', error);
+    });
 }
 
 function displayData(data) {
-    dataContainer.innerHTML = '';
-    data.forEach((item) => {
-        const itemElement = document.createElement("div");
-        itemElement.className = 'route__card';
-        itemElement.innerHTML = `
-            <img src="${item.image}" alt="Image"></img>
-            <h2>${item.name}</h2>
-            <p>${item.description}</p>
-            <a href="#" onclick="openDetailsWindow(${item.id})">Подробнее</a>
-        `;
-        dataContainer.appendChild(itemElement);
-        setTimeout(() => {
-            itemElement.classList.add('visible');
-        }, 50);
-    });
-    updatePagination(data.length > 0);
+  dataContainer.innerHTML = '';
+  data.forEach((item) => {
+    const itemElement = document.createElement("div");
+    itemElement.className = 'route__card';
+    itemElement.innerHTML = `
+      <img src="${item.image}" alt="Image" onclick="openGallery('${item.images.join("','")}')"></img>
+      <h2>${item.name}</h2>
+      <p>${item.description}</p>
+      <a href="?id=${item.id}">Подробнее</a>
+    `;
+    dataContainer.appendChild(itemElement);
+    setTimeout(() => {
+      itemElement.classList.add('visible');
+    }, 50);
+  });
+  updatePagination(data.length > 0);
 }
 
 function updatePagination(hasItems) {
-    paginationContainer.innerHTML = '';
+  paginationContainer.innerHTML = '';
 
-    if (currentPage > 1) {
-        const prevPageButton = document.createElement('button');
-        prevPageButton.className = 'route__pagination-btn';
-        prevPageButton.textContent = '<';
-        prevPageButton.addEventListener('click', () => {
-            currentPage--;
-            fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
-        });
-        paginationContainer.appendChild(prevPageButton);
-    }
+  if (currentPage > 1) {
+    const prevPageButton = document.createElement('button');
+    prevPageButton.className = 'route__pagination-pBtn';
+    prevPageButton.textContent = '<';
+    prevPageButton.addEventListener('click', () => {
+      currentPage--;
+      fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
+    });
+    paginationContainer.appendChild(prevPageButton);
+  }
 
-    if (hasItems) {
-        const nextPageButton = document.createElement('button');
-        nextPageButton.className = 'route__pagination-btn';
-        nextPageButton.textContent = '>';
-        nextPageButton.addEventListener('click', () => {
-            currentPage++;
-            fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
-        });
-        paginationContainer.appendChild(nextPageButton);
-    }
+  if (hasItems) {
+    const nextPageButton = document.createElement('button');
+    nextPageButton.className = 'route__pagination-nBtn';
+    nextPageButton.textContent = '>';
+    nextPageButton.addEventListener('click', () => {
+      currentPage++;
+      fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
+    });
+    paginationContainer.appendChild(nextPageButton);
+  }
 }
 
-searchInput.addEventListener('input', () => {
+searchInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
     const searchTerm = searchInput.value.toLowerCase();
-    const checkbox = document.getElementById('checkbox');
+    currentPage = 1; 
+    fetchData(currentPage, sortBy, order, filterByDistrict, filterByType, searchTerm);
+  }
+});
 
-    let filteredData;
-    if (checkbox.checked) {
-        filteredData = allData.filter(item =>
-            item.description.toLowerCase().includes(searchTerm) ||
-            item.name.toLowerCase().includes(searchTerm)
-        );
-    } else {
-        filteredData = allData.filter(item =>
-            item.name.toLowerCase().includes(searchTerm)
-        );
-    }
-
-    displayData(filteredData);
+searchInput.addEventListener('blur', () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  currentPage = 1; 
+  fetchData(currentPage, sortBy, order, filterByDistrict, filterByType, searchTerm);
 });
 
 function checkFluency() {
-    const checkBox_msg = document.getElementById('checkbox');
-    if (checkBox_msg.checked) {
-        alert("Вы включили поиск по описанию");
-    } else {
-        alert("Вы отключили поиск по описанию");
-    }
+  const checkBox_msg = document.getElementById('checkbox');
+  if (checkBox_msg.checked) {
+    alert("Вы включили поиск по описанию");
+  } else {
+    alert("Вы отключили поиск по описанию");
+  }
 }
 
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 const sortByCategoryBtn = document.getElementById('sortByCategory');
 sortByCategoryBtn.addEventListener('click', () => {
-    sortBy = 'category';
-    order = 'asc';
-    currentPage = 1;
-    fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
+  sortBy = 'category';
+  order = 'asc';
+  currentPage = 1;
+  fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
 });
 
 const sortByRatingBtn = document.getElementById('sortByRating');
 sortByRatingBtn.addEventListener('click', () => {
-    sortBy = 'rating';
-    order = 'desc';
-    currentPage = 1;
-    fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
+  sortBy = 'rating';
+  order = 'desc';
+  currentPage = 1;
+  fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
 });
 
 function openDetailsWindow(itemId) {
-    window.location.href = `?id=${itemId}`;
+  window.location.href = `?id=${itemId}`;
 }
 
 async function fetchAttractions() {
-    const response = await fetch('https://672b170d976a834dd0258e17.mockapi.io/api/v1/tourism');
-    return response.json();
+  const response = await fetch('https://672b170d976a834dd0258e17.mockapi.io/api/v1/tourism');
+  return response.json();
 }
 
 function renderAttractions(attractions) {
-    dataContainer.innerHTML = '';
+  dataContainer.innerHTML = '';
 
-    attractions.forEach(attraction => {
-        const div = document.createElement('div');
-        div.classList.add('route__card');
-        div.innerHTML = `
-        `;
-        dataContainer.appendChild(div);
-    });
+  attractions.forEach(attraction => {
+    const div = document.createElement('div');
+    div.classList.add('route__card');
+    div.innerHTML = `
+      <img src="${attraction.image}" alt="Image" onclick="openGallery('${attraction.images.join("','")}')"></img>
+      <h2>${attraction.name}</h2>
+      <p>${attraction.description}</p>
+      <a href="?id=${attraction.id}">Подробнее</a>
+    `;
+    dataContainer.appendChild(div);
+  });
 }
 
 async function showDetails(attractionId) {
-    const attractions = await fetchAttractions();
-    const attraction = attractions.find(a => a.id === attractionId);
+  const attractions = await fetchAttractions();
+  const attraction = attractions.find(a => a.id === attractionId);
 
-    if (attraction) {
-        dataContainer.style.display = 'none';
-        searchInput.style.display = 'none';
-        dropDown.style.display = 'none';
-        document.getElementById('checkbox').style.display = 'none';
-        document.getElementById('pagination').style.display = 'none';
-        const detailsContainer = document.getElementById('details');
-        detailsContainer.innerHTML = `
-            <h2>${attraction.name}</h2>
-            <div class='route__details-wrapper'>
-                <p>${attraction.descriptions[0]}</p>
-                <img src="${attraction.images[0]}" alt="Image"></img>
-            </div>
-            <div class='route__details-wrapper'>
-                <img src="${attraction.images[1]}" alt="Image"></img>
-                <p>${attraction.descriptions[1]}</p>
-            </div>
-            <h2>Мы на карте</h2>
-            ${attraction.location}
-        `;
-        detailsContainer.classList.remove('hidden');
-    } else {
-        alert('error');
-    }
+  if (attraction) {
+    searchInput.style.display = 'none';
+    dropDown.style.display = 'none';
+    document.getElementById('checkbox').style.display = 'none';
+    document.getElementById('pagination').style.display = 'none';
+    filterBlock.style.display = 'none';
+    dataContainer.style.display = 'none';
+
+    detailsContainer.innerHTML = `
+      <h2>${attraction.name}</h2>
+      <div class='route__details-wrapper'>
+        <p>${attraction.descriptions[0]}</p>
+        <img src="${attraction.images[0]}" alt="Image" onclick="openGallery('${attraction.images.join("','")}')"></img>
+      </div>
+      <div class='route__details-wrapper'>
+        <img src="${attraction.images[1]}" alt="Image" onclick="openGallery('${attraction.images.join("','")}')"></img>
+        <p>${attraction.descriptions[1]}</p>
+      </div>
+      <h2>Мы на карте</h2>
+      ${attraction.location}
+    `;
+    detailsContainer.classList.remove('hidden');
+  } else {
+    alert('error');
+  }
 }
 
 window.onload = async () => {
-    fetchData(currentPage);
-    const params = new URLSearchParams(window.location.search);
-    const attractionId = params.get('id');
-    if (attractionId) {
-        showDetails(Number(attractionId));
-    }
+  fetchData(currentPage);
+  const params = new URLSearchParams(window.location.search);
+  const attractionId = params.get('id');
+  if (attractionId) {
+    showDetails(Number(attractionId));
+  }
 };
 
-document.getElementById('burger').addEventListener('click', function() {
-    const wrapper = document.querySelector('.header__wrapper');
-    wrapper.classList.toggle('active');
-    if (wrapper.classList.contains('active')) {
-        document.querySelector('.route__input-wrapper').style.marginTop = '270px';
-    } else {
-        document.querySelector('.route__input-wrapper').style.marginTop = '0px';
-    }
+document.getElementById('burger').addEventListener('click', function () {
+  const wrapper = document.querySelector('.header__wrapper');
+  wrapper.classList.toggle('active');
+  if (wrapper.classList.contains('active')) {
+    document.querySelector('.route__input-wrapper').style.marginTop = '270px';
+  } else {
+    document.querySelector('.route__input-wrapper').style.marginTop = '0px';
+  }
+});
+
+filterByDistrictSelect.addEventListener('change', () => {
+  filterByDistrict = filterByDistrictSelect.value;
+  currentPage = 1;
+  fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
+});
+
+filterByTypeSelect.addEventListener('change', () => {
+  filterByType = filterByTypeSelect.value;
+  currentPage = 1;
+  fetchData(currentPage, sortBy, order, filterByDistrict, filterByType);
 });
